7fd23a4de42cdce98fd5aafe6cf88b92835a78af
TaDaa <trevor002@gmail.com>
2016-09-28 18:30:22 -0400

    update bar demo with code

--- demos/bars.js
+++ demos/bars.js
@@ -1,21 +1,31 @@
 
-window.addEventListener('load',load)
+window.addEventListener('load',function () {
+    var content = document.querySelector('.content');
+    content.innerHTML = ([ 
+        '<canvas ></canvas>',
+        '<div class="button-group">',
+            '<button style="width:100px" class="add">Double</button>',
+            '<button style="width:100px" class="remove">Half</button>',
+        '</div>',
+        '<pre><code class="javascript"></code></pre>'
+    ].join(''))
+    var str = load.toString();
+    //str = str.slice(str.indexOf('\n'),-1);
+    //str = str.replace(/\n/g)
+    //str = str.replace(/\s/g,'<div style="margin-left:5px;position;relative;display:inline-block;"></div>')
+    //console.error(str);
+    var node = document.querySelector('.javascript');
+    str = str.replace(/\</g,'&lt;')
+    str = str.replace(/\>/g,'&gt;')
+    node.innerHTML = str;
+    hljs.highlightBlock(node.parentNode);
+    load();
+})
 
 
 
 
 function load () {
-    //START_IGNORE
-    var content = document.querySelector('.content');
-    content.innerHTML = ([ 
-        '<canvas style="position:absolute;top:0px;left:0px"></canvas>',
-        '<div style="position:absolute">',
-            '<button style="width:100px" class="add">Double</button>',
-            '<button style="width:100px" class="remove">Half</button>',
-        '</div>',
-    ].join(''))
-    //END_IGNORE
-
 
     //generate mock data
     var data = [],
@@ -31,9 +41,16 @@ function load () {
 
 
     //define our custom canvas element tag
-    //attributes allow c2 to perform eval optimizations so that generic setters can be avoided
+    //attributes allow c2 to perform eval 
+    //optimizations so that generic 
+    //setters can be avoided
     var Rect = c2.element(function (context,d,i) {
-        context.fillRect(this.x,this.y,this.w,this.h);
+        context.fillRect(
+            this.x,
+            this.y,
+            this.w,
+            this.h
+        );
     }).attributes({
         'x' : c2.types.float,
         'y' : c2.types.float,
@@ -42,10 +59,13 @@ function load () {
     });
 
     var direction = 1,
-    width = window.innerWidth,
+    width = window.innerWidth-421,
     height = window.innerHeight,
-    last_size= data.length,
-    canvas = d3.select('canvas').attr('width',width).attr('height',height);
+    canvas = d3.select('canvas')
+        .attr('height',height)
+        .attr('width',width),
+    last_size= data.length;
+
 
 
     //bind event listeners for add/remove buttons
@@ -71,28 +91,42 @@ function load () {
         //log amount of data being rendered
         console.error(data.length);
 
-        var 
-        lastSize = last_size,
-        xScale = d3.scaleLinear().range([0,width]).domain([0,data.length]),
-        yScale = d3.scaleLinear().range([0,height]).domain([0,d3.max(data,function (d) {
+        var lastSize = last_size,
+        xScale = d3.scaleLinear()
+            .range([0,width])
+            .domain([0,data.length]),
+        yScale = d3.scaleLinear()
+            .range([0,height])
+            .domain([0,d3.max(data,function (d) {
             return d.value;
         })]),
 
 
 
-        //we select the context from the canvas element.  c2.Context2d is a predefined element tag -- webgl may be supported later
+        //we select the context from the canvas
+        //c2.Context2d is a predefined element tag 
+        //webgl may be supported later
         context = canvas.select(c2.Context2d)
             .on('tick',function () {
-                this.context.clearRect(0,0,this.canvas.width,this.canvas.height)
+                this.context.clearRect(
+                    0,
+                    0,
+                    this.canvas.width,
+                    this.canvas.height
+                )
             }),
 
-        //we bind the generated data to the custom tag we already created (Rect)
-        selection = context.selectAll(Rect).data(data,function (d,i) {
-            return d.id;
-        });
+        //we bind the generated data
+        //to the custom Rect tag 
+        selection = context.selectAll(Rect)
+            .data(data,function (d,i) {
+                return d.id;
+            });
 
 
-        //this works the same as d3! we append Rect elements and merge them into the selection
+        //this works the same as d3!
+        //we append Rect elements 
+        //and merge them into the selection
         selection.enter().append(Rect)
         .attr('x',function (d,i) {
             return width;
@@ -104,28 +138,35 @@ function load () {
         .attr('h',0)
         .merge(selection)
 
-        //PAUSE! you dont have to to use c2.animate(), but the performance is significantly better over d3-transitions
-        //this is mainly because c2 will dynamically compile custom optimized tweening functions based on the attributes being interpolated.  C2 also groups tweening functions by start/duration/easing.  C2 animate is also easier on garbage collection. I recommend using c2.animate if you are binding large sets of data, but its up to you!!! selection.transition() will work too.
+        //PAUSE! you dont have to to use c2.animate(),
+        //but the performance is significantly
+        //better over d3-transitions.
+        //
+        //This is mainly because c2 will dynamically
+        //compile custom optimized tweening
+        //functions based on the attributes being
+        //interpolated.
+        //
+        //C2 also groups tweening functions by 
+        //start/duration/easing.
+        //
+        //C2 animate is also easier on garbage
+        //collection.   recommend using c2.animate
+        //if you are binding large sets of data,
+        //but its up to you!!!
+        //selection.transition() will work too.
         .call(
             c2.animate()
             .duration(2000)
             .ease(d3.easeElastic)
             .delay(function (d,i) {
                 if (direction === -1) {
-                    return Math.max((lastSize-i)/50,1);
+                    return Math.max(
+                        (lastSize-i)/50,1
+                    );
                 }
                 return Math.max(i/50,1);
             })
-            //.delay(function (d,i) {
-                //return i % 1000
-            //})
-            //.delay(function (d,i) {
-            //if (direction === -1) {
-            //return lastSize-i;
-            //}
-            //return i;
-            //})
-            //define the to animation
             .to({
                 'x' : function (d,i) {
                     return xScale(i);
@@ -134,7 +175,9 @@ function load () {
                     this._y = yScale(d.value);
                     return height-this._y;
                 },
-                'w' : Math.max(width/data.length-0.5,0.1),
+                'w' : Math.max(
+                    width/data.length-0.5,0.1
+                ),
                 'h' : function (d,i) {
                     return this._y;
                 }
@@ -144,18 +187,17 @@ function load () {
 
 
         //nothing new here!
-        //we animate removed data items and remove associated elements
+        //we animate removed data items 
+        //and remove associated elements
         selection.exit()
             .call(
                 c2.animate()
                 .duration(2000)
                 .delay(function (d,i) {
-                    return Math.max((last_size-1)/50,1)
+                    return Math.max(
+                        (last_size-1)/50,1
+                    )
                 })
-                //.delay(function (d,i) {
-                    //return i % 1000;
-                    //return (lastSize-i)
-                //})
                 .ease(d3.easeElastic)
                 .to({
                     'x' : width +100,
